4c0e0359f20d58cf183b32352b65e109d56f1bb0
Raymond Gigliotti <ray@firestitch.com>
2024-10-04 04:23:41 -0400

    fixed demo site

--- playground/app/components/svg/svg.component.ts
+++ playground/app/components/svg/svg.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-svg',
   templateUrl: './svg.component.html',
-  styleUrls: ['svg.component.scss']
+  styleUrls: ['./svg.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SvgComponent {
 
--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,27 +1,26 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { FsExampleModule } from '@firestitch/example';
-import { FsMessageModule } from '@firestitch/message';
-import { FsSignatureModule, FsSignaturePreviewModule, FsSignatureInputModule } from '@firestitch/package';
-import { FsLabelModule } from '@firestitch/label';
 import { FsFormModule } from '@firestitch/form';
-import { ToastrModule } from 'ngx-toastr';
+import { FsLabelModule } from '@firestitch/label';
+import { FsMessageModule } from '@firestitch/message';
+import { FsSignatureInputModule, FsSignatureModule, FsSignaturePreviewModule } from '@firestitch/package';
 
-import { AppMaterialModule } from './material.module';
-import {
-  KitchenSinkComponent,
-  ExamplesComponent,
-  SvgComponent,
-  ReadonlyComponent,
-} from './components';
 import { AppComponent } from './app.component';
-import { KitchenSinkConfigureComponent } from './components/kitchen-sink-configure';
+import {
+  ExamplesComponent,
+  KitchenSinkComponent,
+  ReadonlyComponent,
+  SvgComponent,
+} from './components';
 import { FormsComponent } from './components/forms/forms.component';
+import { KitchenSinkConfigureComponent } from './components/kitchen-sink-configure';
 import { SignatureTypeComponent } from './components/signature-type';
+import { AppMaterialModule } from './material.module';
 
 const routes: Routes = [
   { path: '', component: ExamplesComponent },
@@ -40,7 +39,6 @@ const routes: Routes = [
     FsLabelModule,
     FsExampleModule.forRoot(),
     FsMessageModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
     RouterModule.forRoot(routes, {}),
     FsFormModule,
   ],
